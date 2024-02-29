835b23e91384204e8e54b8e1b2de78f53cc9abd8
nniabb <niabilanishvili17@gmail.com>
2024-02-29 18:32:15 +0400

    fetched images

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import './Mycomponent'
+import MyComponent from './Mycomponent';
 
 function App() {
   return (
     <div className="App">
-      
+      <MyComponent/>
     </div>
   );
 }
